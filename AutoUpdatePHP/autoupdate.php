f1aa8292429b3e465459871cd0e9dd71edce5681
knva <txt_ch@163.com>
2017-05-24 17:03:20 +0800

    php add

--- AutoUpdatePHP/autoupdate.php
+++ AutoUpdatePHP/autoupdate.php
@@ -1,19 +1,8 @@
 <?php
-if (isset($_GET["name"]) && isset($_GET["version"]) && isset($_GET["publicKey"])) {
-	$mname = $_GET["name"];
-	$version = $_GET["version"];
-	$publicKey = $_GET["publicKey"];
-	if ($publicKey == md5('595902716' . '@qq.com')) {
-		$dbh = new PDO("mysql:host=localhost;dbname=update", "root", "root", array(PDO::ATTR_PERSISTENT => true));
-		flushmJson($mname, $version, $dbh);
-		$dbh = null; //(释放)
-		
-	} else {
-		echo 'Are you kidding me?';
-	}
-} else {
-	echo 'You need some salt!';
-}
+//get & send 
+//Parameter1:get name
+//Parameter2:get version
+//Parameter3:pdo handle
 function flushmJson($name, $ver, $dbh) {
 	$userinfo = create_unique();
 	if (!addLog($dbh, $name, $ver, $userinfo)) {
@@ -46,6 +35,12 @@ function flushmJson($name, $ver, $dbh) {
 		return False;
 	}
 }
+
+//Add a log to the database
+//Parameter1:pdo handle
+//Parameter2:update soft name
+//Parameter3:ver
+//Parameter4:user id
 function addLog($dbh, $json, $ver, $userinfo) {
 	$ip = GetIP();
 	$sqlstr = "insert into `updatelog` (`time`,`json`,`ver`,`ip`,`agent`) values ('" . date("Y-m-d H:i:s", time()) . "','" . $json . "','" . $ver . "','" . $ip . "','" . $userinfo . "')";
@@ -56,6 +51,10 @@ function addLog($dbh, $json, $ver, $userinfo) {
 		return False;
 	}
 }
+//Updata log
+//Parameter1:pdo handle
+//Parameter2:update soft result
+//Parameter4:user id
 function updateLog($dbh, $result, $userinfo) {
 	$sqlstr = "UPDATE `updatelog` SET `result` = '" . $result . "' WHERE (`agent`='" . $userinfo . "')";
 	//echo $sqlstr;
@@ -65,11 +64,12 @@ function updateLog($dbh, $result, $userinfo) {
 		return False;
 	}
 }
+//create user id
 function create_unique(){
     $data = $_SERVER['HTTP_USER_AGENT'].$_SERVER['REMOTE_ADDR'].time().rand();
     return sha1($data);//return md5(time().$data);   //return $data;   
 }
-
+//get ip
 function GetIP() {
 	if (getenv("HTTP_CLIENT_IP") && strcasecmp(getenv("HTTP_CLIENT_IP"), "unknown")) $ip = getenv("HTTP_CLIENT_IP");
 	else if (getenv("HTTP_X_FORWARDED_FOR") && strcasecmp(getenv("HTTP_X_FORWARDED_FOR"), "unknown")) $ip = getenv("HTTP_X_FORWARDED_FOR");
@@ -77,5 +77,23 @@ function GetIP() {
 	else if (isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] && strcasecmp($_SERVER['REMOTE_ADDR'], "unknown")) $ip = $_SERVER['REMOTE_ADDR'];
 	else $ip = "unknown";
 	return ($ip);
+}
+//main
+if (isset($_GET["name"]) && isset($_GET["version"]) && isset($_GET["publicKey"])) {
+	$mname = $_GET["name"];
+	$version = $_GET["version"];
+	$publicKey = $_GET["publicKey"];
+	if ($publicKey == md5('595902716' . '@qq.com')) {
+		$dbh = new PDO("mysql:host=localhost;dbname=update;charset=utf8", "root", "root", array(PDO::ATTR_PERSISTENT => true));
+        $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		flushmJson($mname, $version, $dbh);
+		$dbh = null; //(free)
+		
+	} else {
+		echo 'Are you kidding me?';
+	}
+} else {
+	echo 'You need some salt!';
 }
 ?>
